3b398c5c09ad36d48178bcad81bc87f41fa86f65
Cesar Botas <cesar.botas@animaeducacao.com.br>
2025-07-29 00:20:52 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <MenuDrawer v-model:drawer="drawer" />
+
+    <v-app-bar app color="beige">
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>Colinho da Cá</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import MenuDrawer from './components/MenuDrawer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MenuDrawer
+  },
+  data: () => ({
+    drawer: true
+  })
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<!-- src/views/HomeView.vue -->
+<template>
+  <v-container class="fill-height d-flex justify-center align-center">
+    <v-card class="pa-8" elevation="6" color="background" width="100%" max-width="600">
+      <v-card-title class="text-h5 text-center">
+        Bem-vindo ao <strong>Colinho da Cá</strong> 💛
+      </v-card-title>
+      <v-card-text class="text-center mt-4">
+        Seu lugar de confiança para cuidados com seu pet!  
+        Use o menu no canto superior direito para fazer cadastros ou agendamentos.
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style scoped>
+.v-container {
+  background-color: #f5f5dc;
+}
+</style>
